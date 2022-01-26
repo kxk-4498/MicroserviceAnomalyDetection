6fedddefeee09f7f997bb61c126a07508829dbd3
Brian Singer <brian@convolve.com>
2022-01-26 15:28:23 -0500

    fix population/simulation traffic bug

--- README.md
+++ README.md
@@ -6,14 +6,18 @@ It is a mess and we need to cleanup the whole repository
 1. Install minikube
 2. Start minikube: `minikube start`
 3. Deploy SockShop: `bash deploy_sockshop.sh`
-4. Test if it is working `curl http://192.168.49.2:30001` or go to link in browser
+
+Check status of pods: `kubectl get pods --namespace sock-shop`
+
+Test if it is working `curl http://192.168.49.2:30001` or go to link in browser
 
 ## Simulating Traffic on SockShop
 1. cd into `experiment_coordinator`
-2. Setup a conda environment: `conda env create -f environment.yml`
+2. Setup a conda environment: `conda env create -f condaEnv.yml`
 3. Cd into `traffic_simulation`
-If you need to populate the database full of users: `python pop_db.py`
-4. Simulate traffic: `simulate_traffic.py`
+4. Activate conda env: `conda activate sockShop`
+If you need to populate the database full of users: `python simulate_traffic.py -p`
+4. Simulate traffic: `python simulate_traffic.py -t`
 
 ## Collecting Data with Cilium
 1. `bash deploy_cilium.sh`, Brian: installs and deploys cilium, might want a better script
--- experiment_coordinator/traffic_simulation/simulate_traffic.py
+++ experiment_coordinator/traffic_simulation/simulate_traffic.py
@@ -1,3 +1,5 @@
+import argparse
+
 from locust.env import Environment
 from pop_db import PopulateDatabase
 from background_traffic import BackgroundTraffic
@@ -5,18 +7,30 @@ from background_traffic import BackgroundTraffic
 import time
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', dest='populate_db', action='store_true', help='Populate Database')
+    parser.add_argument('-t', dest='sim_traffic', action='store_true', help='Simulate Traffic')
+    parser.set_defaults(populate_db=False)
+    parser.set_defaults(sim_traffic=False)
+    args = parser.parse_args()
+
     # TODO get this automatically or pass it in
     host = 'http://192.168.49.2:30001'
-    # env = Environment(user_classes=[PopulateDatabase], host=host)
-    # env.create_local_runner()
-    # env.runner.start(1, spawn_rate=20)
-    # # Wait 10s
-    # time.sleep(10)
-    # env.runner.stop()
 
-    env = Environment(user_classes=[BackgroundTraffic], host=host)
-    env.create_local_runner()
-    env.runner.start(20, spawn_rate=20)
-    # Wait 120s
-    time.sleep(120)
-    env.runner.stop()
+    if args.populate_db:
+        print('Populating DB...')
+        env = Environment(user_classes=[PopulateDatabase], host=host)
+        env.create_local_runner()
+        env.runner.start(1, spawn_rate=20)
+        # Wait 10s
+        time.sleep(10)
+        env.runner.stop()
+
+    if args.sim_traffic:
+        print('Simulating Traffic...')
+        env = Environment(user_classes=[BackgroundTraffic], host=host)
+        env.create_local_runner()
+        env.runner.start(20, spawn_rate=20)
+        # Wait 120s
+        time.sleep(120)
+        env.runner.stop()
